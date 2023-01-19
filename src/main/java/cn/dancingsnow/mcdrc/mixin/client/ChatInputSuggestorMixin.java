eb8db4338ef132bf8d58bb40e6d4b6d33a73bccf
DancingSnow <1121149616@qq.com>
2023-01-19 15:09:47 +0800

    fix(ChatInputSuggestorMixin): fix can't complete player_list when doesn't have node

--- src/main/java/cn/dancingsnow/mcdrc/mixin/client/ChatInputSuggestorMixin.java
+++ src/main/java/cn/dancingsnow/mcdrc/mixin/client/ChatInputSuggestorMixin.java
@@ -3,6 +3,7 @@ package cn.dancingsnow.mcdrc.mixin.client;
 import cn.dancingsnow.mcdrc.client.MCDRCommandClient;
 import com.mojang.brigadier.suggestion.Suggestions;
 import com.mojang.brigadier.suggestion.SuggestionsBuilder;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.ChatInputSuggestor;
 import net.minecraft.client.gui.widget.TextFieldWidget;
 import net.minecraft.command.CommandSource;
@@ -39,10 +40,12 @@ public abstract class ChatInputSuggestorMixin {
 
     @Shadow @Nullable private ChatInputSuggestor.@Nullable SuggestionWindow window;
 
+    @Shadow @Final private MinecraftClient client;
+
     @Inject(method = "refresh()V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/widget/TextFieldWidget;getCursor()I", shift = At.Shift.AFTER), cancellable = true)
     public void refreshMixin(CallbackInfo ci) {
         String text = this.textField.getText();
-        if (text.startsWith("!")) {
+        if (text.startsWith("!") || text.startsWith("！")) {
             String string = text.substring(0, this.textField.getCursor());
             if (this.window == null || !this.completingSuggestions) {
                 int word = getStartOfCurrentWord(string);
@@ -55,6 +58,9 @@ public abstract class ChatInputSuggestorMixin {
                             this.show(true);
                         }
                     });
+                } else {
+                    Collection<String> player_list = this.client.player.networkHandler.getCommandSource().getChatSuggestions();
+                    this.pendingSuggestions = CommandSource.suggestMatching(player_list, new SuggestionsBuilder(string, word));
                 }
             }
             ci.cancel();
