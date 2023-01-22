3d3204ccfbd298e258ceddee0014203b141a9ad3
DancingSnow <1121149616@qq.com>
2023-01-22 10:18:46 +0800

    fix command agrs

--- src/main/java/cn/dancingsnow/mcdrc/server/NodeReloadCommand.java
+++ src/main/java/cn/dancingsnow/mcdrc/server/NodeReloadCommand.java
@@ -1,21 +1,19 @@
 package cn.dancingsnow.mcdrc.server;
 
 import com.mojang.brigadier.CommandDispatcher;
-import net.minecraft.command.CommandRegistryAccess;
-import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
-import net.minecraft.text.Text;
+import net.minecraft.text.LiteralText;
 
 import static net.minecraft.server.command.CommandManager.literal;
 
 public class NodeReloadCommand {
 
-    public static void register(CommandDispatcher<ServerCommandSource> dispatcher, CommandRegistryAccess registryAccess, CommandManager.RegistrationEnvironment environment) {
+    public static void register(CommandDispatcher<ServerCommandSource> dispatcher, boolean dedicated) {
         dispatcher.register(
                 literal("mcdrc")
                         .requires(serverCommandSource -> serverCommandSource.hasPermissionLevel(2))
                         .then(literal("reload").executes(context -> {
-                            context.getSource().sendFeedback(Text.literal("Reloading nodes..."), true);
+                            context.getSource().sendFeedback(new LiteralText("Reloading nodes..."), true);
                             MCDRCommandServer.loadNodeData();
                             return 1;
                         })));
